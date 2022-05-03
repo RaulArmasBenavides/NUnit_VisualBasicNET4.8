02c41595277876a2d5288b4ae90322bea2fcd631
Raul Armas <you@example.com>
2022-05-03 15:38:21 -0500

    file methods

--- TestingGenericSolutionRA/TestingGenericSolutionnNET4/Bw2.vb
+++ TestingGenericSolutionRA/TestingGenericSolutionnNET4/Bw2.vb
@@ -1,4 +1,7 @@
-﻿Public Class Bw2
+﻿Imports System.IO
+Imports System.Web.Script.Serialization
+
+Public Class Bw2
 
     Public Function GetSerieByYear() As String
         Dim newcode As String = ""
@@ -34,14 +37,6 @@
     Public Function RoundAwayFromZero(ByVal param As Decimal) As Decimal
         Return Math.Ceiling(param)
     End Function
-
-    'Dim GetConstMyDynamicControlDictionary As Dictionary(Of String, String) = Function(x As Dictionary(Of String, String)) x from {{"_COOPERA_Rucknah_Bemerkun_InputControl", "_COOPERA_Rucknahmeverpflichtung_Label"},
-    '                                                                                            {"_COOPERA_sicherheit_Bemerkung_InputControl", "_COOPERA_Hinterlage_sicherheit_Label"},
-    '                                                                                            {"_COOPERA_Nachhal_Bemerkun_InputControl", "_COOPERA_Nachhaltigkeit_Label"},
-    '                                                                                            {"_COOPERA_Eigentumsanzeige_Account_ID_InputControl", "_COOPERA_Eigentumsanzeige_Label"}}
-    'End Function
-
-
 
     Public Function GetConstMyDynamicControlDictionaryoNew() As Dictionary(Of String, String)
         Dim dictionaryControlsExternal As New Dictionary(Of String, String) From {{"_COOPERA_Rucknah_Bemerkun_InputControl", "_COOPERA_Rucknahmeverpflichtung_Label"},
@@ -98,8 +93,64 @@
     Public func3 As Func(Of Integer, String) = Function(x)
 
                                                    Return (x + 1).ToString()
-                                               End Function
+                                                   End Function
+
     ' Call the lambda.
 
+
+
+    Public Function ExecuteFunctionCustom(ByVal data As String, ByVal sourceIp As String) As String
+        Dim result As String = ""
+        Dim jss As New JavaScriptSerializer
+        Dim jsond As Dictionary(Of String, Object) = jss.Deserialize(Of Dictionary(Of String, Object))(data)
+        Dim methodName As String = CStr(jsond("MethodName"))
+
+        Select Case methodName
+            Case "InputFeedJSON" 'New Input JSON
+                result = InputFeedJSON(jsond)
+        End Select
+
+        Return result
+    End Function
+
+
+    Public Function InputFeedJSON(ByVal jsonObj As Dictionary(Of String, Object)) As String
+        Dim TempPath As String = String.Empty
+        Dim FullTempPath As String = String.Empty
+        Dim TargetPath As String = String.Empty
+        Dim FullTargetPath As String = String.Empty
+        Dim msFileName As String = String.Empty
+        Dim _array As Byte()
+        TempPath = "C:\Users\raula\Downloads\move1"
+        TargetPath = "C:\Users\raula\Downloads\target1"
+        Try
+            'if the temp directory not exists ,create a new directory 
+            If Not Directory.Exists(TempPath) Then
+                MkDir(TempPath)
+            End If
+            msFileName = CStr(jsonObj("provider")) + Now.ToString("MM_dd_yyyy_hh_mm_ss") + ".xml"
+            FullTempPath = Path.Combine(TempPath, msFileName)
+            FullTargetPath = Path.Combine(TargetPath, msFileName)
+
+            'save in a temp directory
+            If Not File.Exists(FullTempPath) Then
+                _array = System.Text.Encoding.Default.GetBytes(CStr(jsonObj("payload")))
+                File.WriteAllBytes(FullTempPath, _array)
+            End If
+
+            'create destination file
+            If Not File.Exists(FullTargetPath) Then
+                File.Create(FullTargetPath).Dispose()
+            End If
+
+            File.Copy(FullTempPath, FullTargetPath, True)
+            'copy file to new remote directory 
+            'Shell("cmd.exe /k NET USE \\Servidor\RecursoCompartido /user:Dominio\usuario password")
+
+        Catch ex As Exception
+            Console.WriteLine(ex.Message)
+        End Try
+        Return "SUCCESS"
+    End Function
 
 End Class
--- TestingGenericSolutionRA/TestingGenericSolutionnNET4/Module1.vb
+++ TestingGenericSolutionRA/TestingGenericSolutionnNET4/Module1.vb
@@ -1,22 +1,15 @@
 ﻿Module Module1
-
     Sub Main()
         Dim bo As New Bw2
-        'bo.GetSerieByYear()
-
-        'Dim res As String = String.Empty
-        'res = bo.RemoveSubString("_COOPERA_Schuld_Account01_InputControl".Substring(1), "_InputControl")
-        'Console.WriteLine(res)
-        'Console.WriteLine(bo.RoundAwayFromZero(3.2))
-        'Console.ReadLine()
-        Dim en As Dictionary(Of String, String)
-        en = bo.GetConstMyDynamicControlDictionaryoNew()
-        Console.WriteLine(en)
-        Console.ReadLine()
-
-
-
-        bo.func3.Invoke(3)
+        bo.ExecuteFunctionCustom(HardCodeJSON(), "192.168.1.1")
     End Sub
 
+
+    Public HardCodeJSON As Func(Of String) = Function()
+                                                 Return "{
+	                                                            ""MethodName"": ""InputFeedJSON"",
+	                                                            ""provider"":""PUBT"",
+	                                                            ""payload"": ""xxx""
+                                                         }"
+                                             End Function
 End Module
